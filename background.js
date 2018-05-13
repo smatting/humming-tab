fb204cd91e88d361d67dd2d7096c0827a6aedac0
Stefan Matting <stefan@air>
2018-05-14 00:56:35 +0200

    add a persistent view

--- background.js
+++ background.js
@@ -1,6 +1,45 @@
 
+function openTab() {
+  chrome.tabs.create({url: "tabs.html"}, function(tab) {
+    chrome.storage.local.set({tabsTabId: tab.id});
+  });
+}
+
 function openSearch() {
-  chrome.tabs.create({url: "tabs.html"});
+  chrome.storage.local.get({tabsTabId: null}, function(data) {
+    if (data.tabsTabId) {
+      // chrome.tabs.highlight({tabs: 1});
+      chrome.tabs.get(data.tabsTabId, function(tab) {
+        if (tab) {
+          console.log("found tab", tab);
+          chrome.tabs.highlight({tabs: [tab.index]});
+        } else {
+          openTab();
+        }
+        // tab.windowId
+        // tab.index
+      });
+    } else {
+      openTab();
+    }
+    // console.log('stored tab id', data);
+  });
+
+
+  // const extensionId = chrome.i18n.getMessage("@@extension_id");
+  // const urlPattern = "chrome-extension://" + extensionId + "/tabs.html";
+  // const urlPattern = "*://*/tabs.html";
+  // const urlPattern = "*://*/*";
+  // chrome.tabs.query({}, function (tabs) {
+  //   console.log('tabs', tabs);
+  // });
+
+
+  // chrome-extension://kdaoofjgkmolaccnhjeckkghlgknggif/tabs.html
+  // console.log('id', id);
+  
+  // chrome.tabs.create({url: "tabs.html"});
+  // chrome.tabs.highlight({tabs: 1});
 }
 
 // function updateIcon() {
